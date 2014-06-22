11792936eedb57c9057a3e40e9b8f648e4b8edb0
Rafael Chacon <rchacon@myfitnesspal.com>
2014-06-22 14:45:55 -0700

    Add rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+DockerToy::Application.config.secret_token = 'c439e813c0be85238a3aeff165f3eb135dcdbd396dd4363468f388779be1524ff115adc0b648b889cc31398ef7cb881410b9589893402b30381dcf3211a2fa24'
