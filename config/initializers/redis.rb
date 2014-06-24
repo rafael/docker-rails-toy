ad132bbabcc7c74a97c2f571b651c6864a291725
Rafael Chacon <rchacon@myfitnesspal.com>
2014-06-24 16:57:38 -0700

    Use ENV variable to connect to redis.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-$redis_master = Redis.new(:host => "127.0.0.1",  :port => ENV["REDISMASTER_SERVICE_PORT"])
-$redis_slave = Redis.new(:host => "127.0.0.1",  :port => ENV["REDISSLAVE_SERVICE_PORT"])
+$redis_master = Redis.new(:host => ENV["SERVICE_HOST"],  :port => ENV["REDISMASTER_SERVICE_PORT"])
+$redis_slave = Redis.new(:host => ENV["SERVICE_HOST"],  :port => ENV["REDISSLAVE_SERVICE_PORT"])
